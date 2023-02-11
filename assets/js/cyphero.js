df182f08e876184d30fd40cc76bb81c80868cfa7
piktoll <szrenka.viktor.daniel@gmail.com>
2023-02-11 15:33:16 +0100

    debug 2

--- assets/js/cyphero.js
+++ assets/js/cyphero.js
@@ -70,7 +70,7 @@ window.onload = function () {
             echoEl.innerHTML += `<p>Trying ${num}... Correct answer. Code cracked successfully!</p>`;
         },
         ifValid: function (num) {
-            return (num[0] !== 0 && num.length === 4 && num != null && num != Infinity && app.ifDifferent(num));
+            return (num[0] != 0 && num.length === 4 && num != null && num != Infinity/* && app.ifDifferent(num)*/);
         },
         ifDifferent: function (num) {
             return true;
