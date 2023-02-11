95b4ab162b5a0927e156d1c7df4113f263b02e0d
piktoll <szrenka.viktor.daniel@gmail.com>
2023-02-11 16:33:11 +0100

    GUI will be improved but it should work

--- assets/js/cyphero.js
+++ assets/js/cyphero.js
@@ -11,7 +11,6 @@ window.onload = function () {
         },
         startGame: function () {
             app.mystery = app.generateNumber();
-            console.log(app.mystery);
 
             echoEl.innerHTML += `<p>${app.title} &ndash; ${app.desc}</p><hr />`;
             echoEl.innerHTML += `<p>Welcome to Cyphero! The game is all about cracking a 4-digit code. The code consists of four different digits and the first digit cannot be zero. Happy guessing!</p>`;
@@ -41,28 +40,36 @@ window.onload = function () {
             return Array.from(num.toString(), n => parseInt(n));
         },
         checkInput: function (num) {
-            /* const basis = Array.from(app.mystery.toString(), n => parseInt(n));
-            const check = Array.from(num.toString(), n => parseInt(n)); */
-            const basis = app.numToArray(app.mystery);
-            const check = app.numToArray(num);
+            /*
+                We will only process input if it is valid and all digits are different
+            */
+            if (app.ifValid(num) && app.ifDifferent(num)) {
+                const basis = app.numToArray(app.mystery);
+                const check = app.numToArray(num);
+                let wrongPlace = 0, rightPlace = 0;
 
-            let wrongPlace = 0, rightPlace = 0;
-
-            for (let i = 0; i < check.length; i++) {
-                if (basis.includes(check[i])) {
-                    /*
-                        if the number is included in the basis and is at the same index, we increment the right place,
-                        else if the number is included in the basis but is not at the same index, we increment the wrong place
-                    */
-                    (basis.indexOf(check[i]) == i)
-                        ? rightPlace++
-                        : wrongPlace++;
+                for (let i = 0; i < check.length; i++) {
+                    if (basis.includes(check[i])) {
+                        /*
+                            if the number is included in the basis and is at the same index, we increment the right place,
+                            else if the number is included in the basis but is not at the same index, we increment the wrong place
+                        */
+                        (basis.indexOf(check[i]) == i)
+                            ? rightPlace++
+                            : wrongPlace++;
+                    }
                 }
-            }
 
-            (rightPlace === 4)
-                ? app.playerWon()
-                : echoEl.innerHTML += `<p>Trying ${num}... Numbers that are included but in the wrong place: ${wrongPlace}, numbers that are included and in the right place ${rightPlace}.</p>`;
+                (rightPlace === 4)
+                    ? app.playerWon()
+                    : echoEl.innerHTML += `<p>Trying ${num}... Numbers that are included but in the wrong place: ${wrongPlace}, numbers that are included and in the right place ${rightPlace}.</p>`;
+            } else if (app.ifValid(num)) {
+                /* Not all digits are different but the input is valid nonetheless. */
+                echoEl.innerHTML += `<p>Trying ${num}... Invalid attempt. All digits must be different.</p>`;
+            } else {
+                /* The input is different but invalid. */
+                echoEl.innerHTML += `<p>Trying ${num}... Invalid attempt. The first digit cannot be zero.</p>`;
+            }
 
             inputEl.value = '';
         },
@@ -70,7 +77,7 @@ window.onload = function () {
             echoEl.innerHTML += `<p>Trying ${num}... Correct answer. Congratulations, code cracked successfully!</p>`;
         },
         ifValid: function (num) {
-            return (num[0] != 0 && num.length === 4 && num != null && num != Infinity && app.ifDifferent(num));
+            return (num[0] != 0 && num.length === 4 && num != null && num != Infinity);
         },
         ifDifferent: function (num) {
             return (new Set(app.numToArray(num))).size === app.numToArray(num).length;
@@ -92,12 +99,7 @@ window.onload = function () {
     const buttonEl = document.getElementsByClassName("button-el")[0];
 
     buttonEl.addEventListener("click", function () {
-        /*
-            We will only process input if it is valid and all digits are different
-        */
-        console.log(inputEl.value[0]);
-        if (app.ifValid(inputEl.value))
-            app.checkInput(inputEl.value);
+        app.checkInput(inputEl.value);
     });
 
     app.startGame();
