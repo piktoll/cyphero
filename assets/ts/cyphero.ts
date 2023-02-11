42cb580b7248729e731a507ad03cac8bd92710b2
piktoll <szrenka.viktor.daniel@gmail.com>
2023-02-11 12:43:27 +0100

    Update

--- assets/ts/cyphero.ts
+++ assets/ts/cyphero.ts
@@ -1,6 +1,6 @@
-const echoEl: HTMLElement = document.getElementsByClassName('echo-el')[0];
-const numberEl: HTMLElement = document.getElementsByClassName('number-el')[0];
-const inputEl: HTMLElement = document.getElementsByClassName("input-el")[0];
-const footerEl: HTMLElement = document.getElementsByClassName("footer-el")[0].getElementsByTagName("p")[0];
-const buttonEl: HTMLElement = document.getElementsByClassName("button-el")[0];
+const echoEl = document.getElementsByClassName('echo-el')[0];
+const numberEl = document.getElementsByClassName('number-el')[0];
+const inputEl = document.getElementsByClassName("input-el")[0];
+const footerEl = document.getElementsByClassName("footer-el")[0].getElementsByTagName("p")[0];
+const buttonEl = document.getElementsByClassName("button-el")[0];
 footerEl.innerHTML = `Cyphero CC-BY-SA&nbsp;3.0 2023 Senrima Team.`;
